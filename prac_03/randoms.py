4cc738c944f5732990b8fd17365765da92c74deb
Dong Wentao <wentao.dong@my.jcu.edu.au>
2024-06-08 16:26:08 +0800

    prac_03

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+# Q1: smallest:5 largest:20
+# Q2: smallest:3 largest:9, No. The step is 2, the numbers are 3, 5, 7, or 9.
+# Q3: smallest:2.5 largest:5.5
+import random
+
+print(random.randint(1,100))
